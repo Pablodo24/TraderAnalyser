2eb96eec42180cacbf0ae1920951601727767345
Pablodo24 <ayoubtt170@gmail.com>
2025-03-16 03:44:02 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Trader Analyser App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#121212",
+  },
+  title: {
+    fontSize: 20,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
