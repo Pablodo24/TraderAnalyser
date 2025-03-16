b5477cf6d4a710ad4a70ccbaa3d3845b6cb6250e
Pablodo24 <ayoubtt170@gmail.com>
2025-03-16 05:07:09 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
+export default function Header({ title }) {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>{title}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#1E2923",
+    padding: 15,
+    alignItems: "center",
+  },
+  headerText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
